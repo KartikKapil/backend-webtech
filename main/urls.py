e0ad64fdd88d5c19926c646653d6c55bb6b67627
Kartik Kapil <kkapil58@gmail.com>
2022-04-10 12:08:50 +0530

    Add product done

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Home, Register
+from .views import Home, Register, Add_product
 
 urlpatterns = [
     path('',Home),
     path('register/',Register),
+    path('add_product/',Add_product),
 ]
--- main/views.py
+++ main/views.py
@@ -20,7 +20,19 @@ def Register(request):
         'error': customer_serializer.errors,
     })
 
+@api_view(['POST'])
+def Add_product(request):
+    product_serializer = ProductSerializer(data=request.data)
+    if product_serializer.is_valid():
+        product_serializer.save()
+        return Response(product_serializer.data,status=status.HTTP_201_CREATED)
+    return Response({
+        'error': product_serializer.errors,
+    })
 
+@api_view(['GET'])
+def get_all_products(request):
+    pass
 
 def Home(request):
     return HttpResponse("<h1>basic page setup</h1>")
