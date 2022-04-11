41e40566fa6e926aa5a7e13ef1cf34a880c0653c
Shreya Singh <shreyasingh160730@gmail.com>
2022-04-11 22:17:49 +0530

    Merge branch 'main' of https://github.com/KartikKapil/backend-webtech into second

--- backend/urls.py
+++ backend/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework_simplejwt import views as jwt_views
+from django.conf import settings  
+from django.conf.urls.static import static  
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('token/',jwt_views.TokenObtainPairView.as_view(),name='token_obtain'),
     path('token_refresh/',jwt_views.TokenRefreshView.as_view(),name='token_refresh'),
     path('', include('main.urls')),
-]
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)  
